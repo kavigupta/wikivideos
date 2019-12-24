8daa40f1aec107ed823b16cfc029f5f4349d140c
Kavi Gupta <kavi@globalmbox.com>
2019-12-23 17:24:35 -0800

    add boundaries

--- sentence_tokenizer.py
+++ sentence_tokenizer.py
@@ -9,7 +9,12 @@ sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 class Sentences:
     text = attr.ib()
     sentences = attr.ib()
+    boundaries = attr.ib()
 
     @staticmethod
     def create(text):
-        return Sentences(text, sent_detector.sentences_from_text(text))
+        sentences = sent_detector.sentences_from_text(text)
+        boundaries = [0]
+        for sentence in sentences:
+            boundaries.append(boundaries[-1] + len(sentence))
+        return Sentences(text, sentences, list(zip(boundaries, boundaries[1:])))
